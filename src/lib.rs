7d8789b77244e5b42007fee73ce8c638cd82e86e
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-25 21:46:43 +0300

    Grayscaling an Image

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use base64::decode;
-use web_sys::console::log_1 as log;
 use image::load_from_memory;
+use web_sys::console::log_1 as log;
 
 use wasm_bingen::prelude::*;
 
@@ -12,8 +12,11 @@ pub fn grayscale(encoded_file: &str) {
 
     log(&"Image decoded".into());
 
-    let img = load_from_memory(&base64_to_vector).unwrap();
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
 
-    log(&"Image loaded".into())
-    
+    log(&"Image loaded".into());
+
+    img = img.grayscale();
+
+    log(&"Grayscale effect applied".into());
 }
