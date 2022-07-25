27d18ea9577229e059a0bade7c414a2a844bc972
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-25 21:35:33 +0300

    Base64 Decoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-use wasm_bingen::prelude::*;
+use base64::decode;
 use web_sys::console::log_1 as log;
+
+use wasm_bingen::prelude::*;
 
 #[wasm_bingen]
 pub fn grayscale(encoded_file: &str) {
-  log(&encoded_file.into())
-    
+    log(&"Grayscale scalled".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+
+    log(&"Image decoded".into())
 }
